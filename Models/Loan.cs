db278c4d317460804db979f90d2a7e20677b66c9
zuranimendoza28 <zuranimendoza@gmail.com>
2024-07-29 18:09:52 -0500

    implementation of required with message

--- Models/Loan.cs
+++ Models/Loan.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ServeBooks.Models
 {
     public class Loan
@@ -5,8 +7,17 @@ namespace ServeBooks.Models
         public int Id { get; set; }
         public int UserID { get; set; }
         public int BookId { get; set; }
+        
+
+        [Required(ErrorMessage = "Loan Date  is required.")]
         public DateTime LoanDate { get; set; }
+
+
+        [Required(ErrorMessage = "Return Date  is required.")]
         public DateTime ReturnDate { get; set; }
+
+        
+        [Required(ErrorMessage = "Status is required.")]
         public string? Status { get; set; }
 
         public User? User { get; set; }
