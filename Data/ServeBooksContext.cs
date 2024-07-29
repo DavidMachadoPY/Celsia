a62477393f7021efb06d5a15c0d25022dc96595d
Juan Pablo <juanpablolc414@gmail.com>
2024-07-29 16:15:21 -0500

    Dbset of models

--- Data/ServeBooksContext.cs
+++ Data/ServeBooksContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ServeBooks.Models;
 
 namespace ServeBooks.Data
 {
@@ -11,5 +12,8 @@ namespace ServeBooks.Data
         public ServeBooksContext(DbContextOptions<ServeBooksContext> options) : base (options){
             
         }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
