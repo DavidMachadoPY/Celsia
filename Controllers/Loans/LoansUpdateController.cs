ccfd5c1cf269fae8dde0cd8c6f8ddea5aa91c50d
KennyVell <kenneth.geme1@gmail.com>
2024-07-30 15:25:24 -0500

    approve reject loan, change status loand and book

--- Controllers/Loans/LoansUpdateController.cs
+++ Controllers/Loans/LoansUpdateController.cs
@@ -9,7 +9,6 @@ namespace ServeBooks.Controllers.Loans
 {
     /*[ApiController]
     [Route("api/[controller]")]*/
-    [Authorize(Roles = "Admin")]
     public class LoansUpdateController : ControllerBase
     {
         private readonly ILoansRepository _repository;
@@ -20,6 +19,7 @@ namespace ServeBooks.Controllers.Loans
 
         [HttpPut]
         [Route("api/loans/{id}")]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Update(int id, [FromBody] LoanDTO loanDto)
         {
             if (!ModelState.IsValid)
